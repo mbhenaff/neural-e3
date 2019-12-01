f35253e89280aeb1855f313e3fda5837946e3398
Mikael <mikael314@gmail.com>
2019-12-01 22:50:38 +0000

    update

--- baselines2/DeepRL/baselines/homer/test.py
+++
@@ -1,9 +0,0 @@
-import torch
-import rl_acid_environment
-import environment_wrapper
-
-
-config = {"horizon": 3}
-config["obs_dim"] = 4*config["horizon"]
-env = environment_wrapper.GenerateEnvironmentWrapper(env_name, config)
-
--- baselines2/DeepRL/docker_build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-nohup docker build -t deep_rl/v1.1 . &
